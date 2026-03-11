a0086948767aacc3e6be59a200fe86f73855aeca
Siyabonga Madondo <MDNSIY014@myuct.ac.za>
2026-03-11 21:25:25 +0200

    Add lexical analyser for BLA programming language

---
+++ src/lex_bla.py
@@ -0,0 +1,69 @@
+import ply.lex as lex
+import sys
+
+"""
+Lexical Analyser which does lexical analysis for the BLA programming language.
+:author: Siyabonga Madondo
+:version: 18/03/2026
+"""
+
+# Token Definitions
+tokens = [
+    'ID',
+    'BINARY_LITERAL',
+    'WHITESPACE',
+    'COMMENT',
+    'EQUALS',
+    'LPAREN',
+    'RPAREN',
+    'A',
+    'S',
+    'M',
+    'D',
+]
+
+# Regular Expressions
+t_ID = r'[a-z_][A-Za-z0-9_]*'
+t_BINARY_LITERAL = r'[+-]?[01]+'
+t_WHITESPACE = r'\s+'
+t_COMMENT = r'//.*|/\*[\s\S]*?\*/'
+t_EQUALS = r'='
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_A = r'A'
+t_S = r'S'
+t_M = r'M'
+t_D = r'D'
+
+# Error Function
+def t_error(t):
+    t.lexer.skip(1)
+
+# Input File Handling
+input_file = sys.argv[1]
+content = ''
+
+with open(input_file, 'r') as file:
+    content = file.read()
+
+# Lexer Initialisation
+lexer = lex.lex()
+lexer.input(content)
+
+# Output File Handling
+output_file = input_file.replace('.bla', '.tkn')
+
+with open(output_file, 'w') as file:
+    while True:
+        token = lexer.token()
+        if not token:
+            break
+        elif token.type in {'WHITESPACE', 'COMMENT'}:
+            print(f"{token.type}")
+            file.write(f"{token.type}\n")
+        elif token.type in {'EQUALS', 'LPAREN', 'RPAREN', 'A', 'S', 'M', 'D'}:
+            print(f"{token.value}")
+            file.write(f"{token.value}\n")
+        else:
+            print(f"{token.type},{token.value}")
+            file.write(f"{token.type},{token.value}\n")
